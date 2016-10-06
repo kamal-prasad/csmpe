f937a092de76980c3c5f2402698894ba8b463d24
Joy Dai <yijoydai@gmail.com>
2016-10-06 12:02:42 -0700

    adding the single quote in csm_pm that someone accidentally deleted..

--- csmpe/csm_pm.py
+++ csmpe/csm_pm.py
@@ -36,7 +36,7 @@ from context import PluginContext
 
 install_phases = ['Pre-Upgrade', 'Pre-Add', 'Add', 'Pre-Activate', 'Activate', 'Pre-Deactivate',
                   'Deactivate', 'Pre-Remove', 'Remove', 'Commit', 'Get-Software-Packages',
-                  'Hardware-Audit', 'Pre-Migrate', 'Migrate', 'Post-Migrate', 'Post-Upgrade', 'FPD-Upgrade]
+                  'Hardware-Audit', 'Pre-Migrate', 'Migrate', 'Post-Migrate', 'Post-Upgrade', 'FPD-Upgrade']
 
 auto_pre_phases = ["Add", "Activate", "Deactivate"]
 
